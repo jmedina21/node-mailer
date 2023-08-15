c24a0119c2c93fd6178cdcbb3afb97b28577787c
Pablo Medina <jmedinamulet@gmail.com>
2023-08-15 14:35:20 -0400

    auto gif email

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
-const express = require('express');
-const app = express();
 const nodeMailer = require('nodemailer');
 require('dotenv').config();
+const puppeteer = require('puppeteer');
+
+let giphList = [];
+
+const giph = async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://giphy.com/search/love');
+    await page.waitForSelector('div.giphy-grid div:first-of-type a img.giphy-gif-img');
+
+    const scrollDown = async () => {
+        await page.evaluate(() => {
+          window.scrollBy(0, window.innerHeight);
+        });
+      };
+    
+      for (let i = 0; i < 2; i++) {
+        await scrollDown();
+        await page.waitForTimeout(1000);
+      }
+
+      await page.waitForSelector('img.giphy-gif-img.giphy-img-loaded')
+
+    const giph = await page.evaluate(() => {
+        const images = Array.from(document.querySelectorAll('img.giphy-gif-img.giphy-img-loaded')).map((image) => image.src);
+        return images.slice((images.length/2 + 1), images.length);
+    });
+    
+    giphList.push(giph);
+    await browser.close();
+};
 
 const transporter = nodeMailer.createTransport({
     service: 'hotmail',
@@ -11,23 +40,43 @@ const transporter = nodeMailer.createTransport({
     },
 });
 
-const mailOptions = {
-    from: process.env.MY_EMAIL,
-    to: 'jmedinamulet@gmail.com',
-    subject: 'Sending Email using Node.js',
-    text: 'That was easy!',
+const gifLink = async () => {
+    await giph()
+    const randomIndex = Math.floor(Math.random() * giphList[0].length)
+    return giphList[0][randomIndex];
 };
 
-transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Email sent:' + info.response);
+const getEmailContent = async () => {
+    const gifSrc = await gifLink();
+    return `
+      <h1>Hello!</h1>
+      <p>Check out this cool GIF:</p>
+      <img src="${gifSrc}" alt="Cool GIF">
+    `;
+};
+
+const sendEmail = async () => {
+    try {
+        const emailContent = await getEmailContent();
+        
+        const mailOptions = {
+            from: process.env.MY_EMAIL,
+            to: process.env.EMAIL_TO,
+            subject: 'Sending gif',
+            html: emailContent
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent:' + info.response);
+            }
+        });
+
+    } catch (error) {
+        console.error("Failed to send email:", error);
     }
-});
+};
 
-
-
-
-
-app.listen(3000, () => console.log('Server started on', 3000));
+sendEmail();
